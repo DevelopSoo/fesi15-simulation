c67805d49f53792b29048a0d9d079094503eb8c6
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-06-05 14:34:58 +0900

    e2e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <MSWProvider>
-          <h1>prerendering</h1>
           <QueryProvider>{children}</QueryProvider>
         </MSWProvider>
       </body>
