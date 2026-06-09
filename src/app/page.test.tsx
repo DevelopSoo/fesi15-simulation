cffb98f0b03b4497944ee8ca71d9f27aa4aca240
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-06-09 11:31:25 +0900

    실패 테스트

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,9 +1,8 @@
 // src/app/page.test.tsx
 // 게시글 상세 페이지 테스트 파일입니다.
 
-import { render, screen } from "@testing-library/react";
-import PostDetailPage from "./page";
-
 describe("게시글 상세 페이지", () => {
-  test("게시글 상세 데이터를 불러와 화면에 렌더링한다", async () => {});
+  test("게시글 상세 데이터를 불러와 화면에 렌더링한다", async () => {
+    expect(1).toBe(2);
+  });
 });
